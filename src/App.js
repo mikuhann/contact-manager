f32cfc5a90f7481d6623df806e712809fcea9504
Mikuhann <ng.kano@gmail.com>
2019-07-01 16:57:45 +0300

    add bootstrap, proptypes header component contact component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+import Contact from './components/Contact';
+import Header from './components/Header';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>The App component</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header title='Contact manager'/>
+        <Contact name='John Doe' email='john@email.com' phone='555-44-33'/>
+      </div>
+    )
+  }
 }
 
 export default App;
